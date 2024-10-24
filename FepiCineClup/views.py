4eea1d1878932d6d59540a61a9e9de898f60a7d4
GuilhermeRenno <guilherme.silveirareno@gmail.com>
2024-10-24 15:48:10 -0300

    att

--- FepiCineClup/views.py
+++ FepiCineClup/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.templatetags.static import static  # Adicione isso
+from django.templatetags.static import static  
 
 def home(request):
     return render(request, 'home.html')
@@ -7,28 +7,27 @@ def home(request):
 def movie_detail(request):
     film_id = request.GET.get('filmId')
     
-    # Dicionário de dados do filme, incluindo o caminho da imagem
     filmes = {
         '1': {
-            'titulo': 'Interstellar',
+            'titulo': 'Interestelar',
             'ano': 2014,
-            'genero': 'Ficção Científica',
-            'resumo': 'Um grupo de astronautas viaja por um buraco de minhoca em busca de um novo lar para a humanidade.',
-            'imagem': static('FepiCineClup/images/EntreEstrelas.jpg')  # Corrigido
+            'genero': 'Ficção Científica, Aventura',
+            'resumo': 'Em um futuro próximo, a Terra enfrenta a extinção devido a catástrofes ecológicas. Cooper, um ex-piloto da NASA, é recrutado para uma missão intergaláctica que busca um novo lar para a humanidade. Junto com uma equipe de astronautas, ele atravessa um buraco de minhoca em busca de planetas habitáveis. O filme explora temas como o amor, a sobrevivência e a relatividade do tempo, enquanto desafia os limites da física e da compreensão humana.',
+            'imagem': static('FepiCineClup/images/EntreEstrelas.jpg')  
         },
         '2': {
             'titulo': 'Jurassic Park',
             'ano': 1993,
-            'genero': 'Aventura/Ficção',
-            'resumo': 'Um parque com dinossauros clonados enfrenta o caos quando as criaturas escapam de seus recintos.',
-            'imagem': static('FepiCineClup/images/JurasQuePark.jpg')  # Corrigido
+            'genero': 'Aventura, Ficção Científica',
+            'resumo': '"Jurassic Park" segue a história de um parque temático onde dinossauros clonados são trazidos à vida através da engenharia genética. Quando os sistemas de segurança do parque falham, os dinossauros escapam e ameaçam a vida dos visitantes e da equipe do parque. Com um elenco liderado por Alan Grant e Ellie Sattler, o filme é uma mistura de suspense, ação e reflexão sobre os limites da ciência e a natureza.',
+            'imagem': static('FepiCineClup/images/JurasQuePark.jpg')  
         },
         '3': {
-            'titulo': 'O Paizão',
-            'ano': 1999,
-            'genero': 'Comédia',
-            'resumo': 'Um homem imaturo adota um menino para impressionar sua namorada, mas acaba se apegando a ele.',
-            'imagem': static('FepiCineClup/images/Opaizao.jpg')  # Corrigido
+            'titulo': 'O Poderoso Chefão',
+            'ano': 1972,
+            'genero': 'Crime, Drama',
+            'resumo': 'O Poderoso Chefão" narra a história da família mafiosa Corleone, liderada por Don Vito Corleone, um dos chefes do crime mais respeitados de Nova York. Quando seu filho Michael, um ex-militar, é forçado a se envolver nos negócios da família após um atentado contra seu pai, ele mergulha em um mundo de crime, violência e poder. O filme explora temas de lealdade, justiça e a luta pelo controle de um império criminoso',
+            'imagem': static('FepiCineClup/images/Opaizao.jpg')  
         }
     }
 
@@ -37,5 +36,4 @@ def movie_detail(request):
     if filme:
         return render(request, 'movie_detail.html', {'filme': filme})
     else:
-        # Lida com o caso de filme não encontrado
         return render(request, '404.html')
